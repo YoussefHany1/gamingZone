9412cd5108f9bcb23dac6fe51fdaee13285f80df
Youssef Hany <youssefhany.2005.yh@gmail.com>
2026-06-19 19:33:31 +0300

    refactor: remove obsolete configuration and source files, streamlining project structure

--- backend/scripts/fetchRss.cjs
+++
@@ -1,8 +0,0 @@
-const { runFetchRss } = require("./fetchRss.service.cjs");
-
-runFetchRss()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error("Fatal Error:", error);
-    process.exit(1);
-  });
